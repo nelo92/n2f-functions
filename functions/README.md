e9358a33ed0ed9f118ffa24bbeb196b32ed50482
manuel <nelo92@hotmail.com>
2020-10-26 23:17:55 +0100

    export format date fr

--- functions/README.md
+++ functions/README.md
@@ -14,3 +14,6 @@ http://localhost:5001/nodedefrais-functions/us-central1/api
 
 Create and Deploy Your First Cloud Functions
 https://firebase.google.com/docs/functions/write-firebase-functions
+
+// /_ eslint-disable promise/catch-or-return _/
+// /_ eslint-disable promise/always-return _/
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,3 @@
-// /* eslint-disable promise/catch-or-return */
-// /* eslint-disable promise/always-return */
-
 const functions = require('firebase-functions');
 const logger = functions.logger;
 
@@ -22,6 +19,8 @@ const excel = require("exceljs");
 
 app.use(cors({ origin: true }));
 
+const DATE_FORMAT = "dd/MM/yyyy";
+
 const XLS_SHEET_NAME = "N2F";
 const XLS_HEADER_DATE = "Date";
 const XLS_HEADER_AMOUNT = "Amount";
@@ -32,112 +31,128 @@ const FIREBASE_COLLECTION = "datas";
 const FIREBASE_FIELD_DATE = "date";
 const FIREBASE_FIELD_AMOUNT = "amount";
 
+const moment = require('moment');
+moment().locale('fr');
+
 app.get('/isUp', (req, res) => {
     res.sendStatus(200);
 });
 
 app.get('/exportAll', (req, res) => {
-
-    logger.info("exportAll");
-
-    let datas = getDatas();
-
-    for (let i = 0; i < datas.length; i += 1) {
-        logger.info("date=" + datas[i].date);
-        logger.info("amount=" + datas[i].amount);
-    }
-
-    createXlsWithDatas(res, datas);
-
-    // const colRef = firestore.collection(FIREBASE_COLLECTION);
-    // // var query = colRef.orderBy(FIREBASE_FIELD_DATE, Query.Direction.ASCENDING);
-    // // query.get()
-    // const snapshot = colRef.get();
-    // snapshot.then(querySnapshot => {
-    //     querySnapshot.docs.forEach(doc => {
-    //         logger.info("date: " + doc.get(FIREBASE_FIELD_DATE));
-    //         logger.info("amount: " + doc.get(FIREBASE_FIELD_AMOUNT));
-    //     });
-    // });
-
-    // snapshot.then(querySnapshot => {
-    //     querySnapshot.docs.forEach(doc => {
-    //         logger.info("doc ; " + doc.data());
-    //     });
-    // }).catch((e) => {
-    //     logger.error(e);
-    // });
-
-    // logger.info("create array datas ...");
-    // let datas = [];
-    // datas.push({ date: "my_date", amount: "my_amount" });
-    // for (let i = 0; i < datas.length; i += 1) {
-    //     logger.info("date=" + datas[i].date);
-    //     logger.info("amount=" + datas[i].amount);
-    // }
-    // logger.info("create array datas.");
-
-    // createXls(res);
-    res.sendStatus(200);
+    logger.info("exportAll...");
+    loadDatas(res, {});
+    logger.info("exportAll.");
 });
 
 app.get('/export/:year/:month', (req, res) => {
+    logger.info("export...");
     var year = req.params.year;
     var month = req.params.month;
     logger.info("year=", year);
     logger.info("month=", month);
-    res.sendStatus(200);
+    loadDatas(res, createFilter(year, month));
+    logger.info("export.");
 });
 
-app.get('/test', (req, res) => {
-    createXls(res);
-});
+function logDate(d) {
+    let date = ("0" + d.getDate()).slice(-2);
+    let month = ("0" + (d.getMonth() + 1)).slice(-2);
+    let year = d.getFullYear();
+    logger.info(" date ->", d);
+    logger.info(" date.toLocaleString ->", d.toLocaleString());
+    return date + "/" + month + "/" + year;
+}
 
-function getDatas() {
+function formatDate(d) {
+    let date = ("0" + d.getDate()).slice(-2);
+    let month = ("0" + (d.getMonth() + 1)).slice(-2);
+    let year = d.getFullYear();
+    return date + "/" + month + "/" + year;
+}
+
+
+function createFilter(year, month) {
+    let start = new Date(year, month - 1, 1);
+    start.setHours(0, 0, 0, 0);
+    let end = new Date(year, (month - 1) + 1, 1);
+    end.setHours(0, 0, 0, 0);
+    return { start: start, end: end };
+}
+
+function loadDatas(res, filter) {
+    logger.info("loadDatas...");
     let datas = [];
-    const colRef = firestore.collection(FIREBASE_COLLECTION);
-    // var query = colRef.orderBy(FIREBASE_FIELD_DATE, Query.Direction.ASCENDING);
-    // query.get()
-    const snapshot = colRef.get();
+    let query = firestore.collection(FIREBASE_COLLECTION);
+
+    logger.info("filter.start=", logDate(filter.start));
+    logger.info("filter.end=", logDate(filter.end));
+
+    if (filter.start && filter.end) {
+        query = query
+            .where("date", ">=", filter.start)
+            .where("date", "<", filter.end);
+    }
+    query = query.orderBy(FIREBASE_FIELD_DATE, "asc");
+    const snapshot = query.get();
     snapshot.then(querySnapshot => {
         querySnapshot.docs.forEach(doc => {
+            let d = doc.get(FIREBASE_FIELD_DATE).toDate();
+            let m = new moment(d).locale("fr");
             datas.push({
-                date: doc.get(FIREBASE_FIELD_DATE),
+                date: m.format("L"),
                 amount: doc.get(FIREBASE_FIELD_AMOUNT)
             });
-
         });
-        return datas;
+        createXls(res, datas);
+        return;
     }).catch(error => {
-        return datas;
+        logger.log("Error: ", error);
+        res.sendStatus(500);
+        return;
     });
-    return datas;
+    logger.info("loadDatas.");
 }
 
-// function log(datas) {
-//     for (let i = 0; i < datas.length; i += 1) {
-//         logger.info("datas");
-//     }
-// }
+function logDatas(datas) {
+    logger.info("log...");
+    logger.info("datas length :", datas.length);
+    for (let i = 0; i < datas.length; i += 1) {
+        logger.info("date=", datas[i].date);
+        logger.info("amount=", datas[i].amount);
+    }
+    logger.info("log.");
+}
 
-function createXls(res) {
-    logger.info("createXls");
+function createXls(res, datas) {
+    logger.info("createXls...");
+
+    // logDatas(datas);
+
     const workbook = new excel.Workbook();
-    workbook.addWorksheet(XLS_SHEET_NAME);
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename=' + XLS_FILE_NAME);
-    workbook.xlsx.write(res);
-}
+    const worksheet = workbook.addWorksheet(XLS_SHEET_NAME);
 
-function createXlsWithDatas(res, datas) {
-    logger.info("createXls");
-    const workbook = new excel.Workbook();
-    workbook.addWorksheet(XLS_SHEET_NAME);
-    res.setHeader('Content-Type', 'application/octet-stream');
-    res.setHeader('Content-Disposition', 'attachment; filename=' + XLS_FILE_NAME);
-    workbook.xlsx.write(res);
-}
+    // row header
+    let row = worksheet.addRow([XLS_HEADER_DATE, XLS_HEADER_AMOUNT]);
+    row.font = { bold: true };
 
+    // row data
+    for (let i = 0; i < datas.length; i += 1) {
+        let row = worksheet.addRow([datas[i].date, datas[i].amount]);
+        row.getCell(1).numFmt = "dd/mm/yyyy";
+        row.getCell(2).numFmt = "0.00";
+        row.getCell(2).alignment = { horizontal: 'right' };
+    }
+    // create file
+    res.setHeader('Content-Disposition', 'attachment; filename=' + XLS_FILE_NAME);
+    res.setHeader('Content-Type', 'application/octet-stream');
+
+    // set column with
+    worksheet.getColumn(1).width = 12;
+    worksheet.getColumn(2).width = 12;
+
+    workbook.xlsx.write(res);
+    logger.info("createXls.");
+}
 
 const api = functions.https.onRequest(app);
 
